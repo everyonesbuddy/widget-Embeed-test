908fdc5a459754076cb0e19753999eb962139da2
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2024-12-22 14:39:39 -0500

    z index addition

--- src/Widget/Widget.jsx
+++ src/Widget/Widget.jsx
@@ -9,22 +9,24 @@ const Widget = () => {
   };
 
   return (
-    <div className="widget-container">
-      {!isOpen ? (
-        <button className="widget-button" onClick={toggleWidget}>
-          Open Widget
-        </button>
-      ) : (
-        <div className="widget-modal">
-          <button className="widget-close" onClick={toggleWidget}>
-            &times;
+    <div id="react-widget">
+      <div className="widget-container">
+        {!isOpen ? (
+          <button className="widget-button" onClick={toggleWidget}>
+            Open Widget
           </button>
-          <div className="widget-content">
-            <h2>Welcome to the Widget!</h2>
-            <p>This is your custom widget content.</p>
+        ) : (
+          <div className="widget-modal">
+            <button className="widget-close" onClick={toggleWidget}>
+              &times;
+            </button>
+            <div className="widget-content">
+              <h2>Welcome to the Widget!</h2>
+              <p>This is your custom widget content.</p>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
